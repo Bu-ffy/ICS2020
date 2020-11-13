3d810a951525d0d43984d2e660d216635648f182
191240004-Chen Bofan <191240004@smail.nju.edu.cn>
2020-11-13 19:37:22 +0800

    2020.11.13

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,54 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+ 
+//单步执行
+static int cmd_si(char *args){
+  if (args == NULL){
+    cpu_exec(1);
+    return 0;
+  }else{
+    int n=0;
+    while( sscanf(args,"%d",&n) != EOF){
+      cpu_exec(n);
+      return 0;
+    };
+    printf("Input format error!");
+  }
+}
+
+static void print_regster(){
+  for(int i=0;i<8;i++){
+    printf("%s\t%#012X\t%d\n",reg_name(i,4),reg_l(i),reg_l(i));
+  }
+  for(int i=0;i<8;i++){
+    printf("%s\t%#012X\t%d\n",reg_name(i,2),reg_w(i),reg_w(i));
+  } 
+  for(int i=0;i<8;i++){
+    printf("%s\t%#012X\t%d\n",reg_name(i,1),reg_b(i),reg_b(i));
+  }
+}
+
+//打印寄存器
+static int cmd_info(char *args){
+  if(args == NULL){
+    printf("Input format error!");
+    return 0;
+  }
+  switch(*args){
+    case 'r':
+      print_regster();
+      break;
+    case 'w':
+
+      break;
+  }
+}
+
+//扫描内存
+static int cmd_scan(int num,char *str){
+  
+}
 
 static struct {
   char *name;
@@ -49,7 +97,9 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
-
+  {"si", "Single instruction execuiton", cmd_si },      //单步执行
+  {"info", "Print the regsters or watchpoints", cmd_info },    //打印寄存器状态或者监视点信息
+  {"x", "Scan the memory", cmd_scan }       //扫描内存
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
